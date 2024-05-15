dbaa4ec3f9fd79bb6f8622a170f1bec68ae1e8ad
RubensAugustoMM <rubensammiranda@gmail.com>
2024-05-15 17:44:04 -0300

    corrigido espaços errados e indentação

--- Cod3rsGrowth.Dominio/PorteEnum.cs
+++ Cod3rsGrowth.Dominio/PorteEnum.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.ComponentModel;
+using System.ComponentModel
 
 namespace Cod3rsGrowth.Dominio;
-public enum PorteEnum 
+public enum PorteEnum
 {
     [Description("Microempreendedor Individual")]
-     MicroeempreendedorIndividual,
-    [Description("Micro Empresa")] 
-     Microempresa, 
+    MicroeempreendedorIndividual,
+    [Description("Micro Empresa")]
+    Microempresa,
     [Description("Empresa de Pequeno Porte")]
-     EmpresaPequenoPorte,
-    [Description("Media Empresa")] 
-     MediaEmpresa, 
+    EmpresaPequenoPorte,
+    [Description("Media Empresa")]
+    MediaEmpresa,
     [Description("Grande Empresa")]
-     GrandeEmpresa 
+    GrandeEmpresa
 }
