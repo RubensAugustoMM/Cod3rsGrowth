07ef31c892bc40268da7c019153ef6cb7bd13140
RubensAugustoMM <rubensammiranda@gmail.com>
2024-05-16 16:49:52 -0300

    Adiciona o  modulo injetor e a classe TesteBase

---
+++ Cod3rsGrowth.Testes/ModuloInjetor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit.Microsoft.DependencyInjection;
+using System;
+using Microsoft.VisualBasic;
+using System.Collections;
+
+namespace Cod3rsGrowth.Testes;
+
+public class ModuloInjetor
+{
+    private ServiceCollection _Servicos;
+
+    public ModuloInjetor()
+    {
+        _Servicos = new ServiceCollection();
+    }
+
+    public ServiceProvider ConstroiServiceProvider()
+    {
+        return _Servicos.BuildServiceProvider();
+    }
+
+}
---
+++ Cod3rsGrowth.Testes/TesteBase.cs
@@ -0,0 +1,23 @@
+﻿using Xunit.Microsoft.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel;
+using System;
+
+namespace Cod3rsGrowth.Testes;
+
+public class TesteBase : IDisposable
+{
+    protected ServiceProvider serviceProvider { get; set; }
+   
+    public TesteBase()
+    {
+        var ModuloInjetor = new ModuloInjetor();
+        serviceProvider = ModuloInjetor.ConstroiServiceProvider();
+    }
+     
+    public void Dispose()
+    {
+        serviceProvider.Dispose(); 
+    }
+
+}
