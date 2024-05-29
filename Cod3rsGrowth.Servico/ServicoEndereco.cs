22d752b17e1289dc4aa24638d17d3eac8c7916bf
RubensAugustoMM <rubensammiranda@gmail.com>
2024-05-29 14:10:53 -0300

    corrige erros no arquivo ServicoEndereco

--- Cod3rsGrowth.Servico/ServicoEndereco.cs
+++ Cod3rsGrowth.Servico/ServicoEndereco.cs
@@ -29,30 +29,4 @@ public class ServicoEndereco : IRepositorioEndereco
     {
         throw new NotImplementedException();
     }
-public class ServicoEndereco : IRepositorioEndereco
-{
-    public void Atualizar(Endereco endrecoAtualizado)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Criar(Endereco enderecoCriado)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Deletar(int Id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Endereco ObterPorId(int Id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public List<Endereco> ObterTodos()
-    {
-        throw new NotImplementedException();
-    }
 }
