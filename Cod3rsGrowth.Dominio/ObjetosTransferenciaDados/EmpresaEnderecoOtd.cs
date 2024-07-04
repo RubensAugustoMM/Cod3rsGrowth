6004cf5bab418fd6181105b9368481de27ab2686
RubensAugustoMM <rubensammiranda@gmail.com>
2024-07-04 13:52:54 -0300

    task-criar-tela-de-lista: cria objetos de transferência de dados  /# time 0h 16m

---
+++ Cod3rsGrowth.Dominio/ObjetosTransferenciaDados/EmpresaEnderecoOtd.cs
@@ -0,0 +1,19 @@
+﻿namespace Cod3rsGrowth.Dominio.ObjetosTranferenciaDados;
+
+public class EmpresaEnderecoOtd
+{ 
+    public int Id { get; set; }
+    public string RazaoSocial { get; set; }
+    public string NomeFantasia { get; set; }
+    public string Cnpj { get; set; }
+    public bool SitucaoCadastral { get; set; }
+    public DateTime DataSituacaoCadastral { get; set; } = new();
+    public int Idade { get; set; }
+    public DateTime DataAbertura { get; set; } = new();
+    public string NaturezaJuridica { get; set; }
+    public string Porte { get; set; }
+    public string MatrizFilial { get; set; }
+    public decimal CapitalSocial { get; set; }
+    public int IdEndereco { get; set; }
+    public string Estado { get; set; }
+}
---
+++ Cod3rsGrowth.Dominio/ObjetosTransferenciaDados/EscolaEnderecoOtd.cs
@@ -0,0 +1,16 @@
+﻿namespace Cod3rsGrowth.Dominio.ObjetosTranferenciaDados;
+
+public class EscolaEnderecoOtd
+{
+    public int Id { get; set; }
+    public bool StatusAtividade { get; set; }
+    public string Nome { get; set; }
+    public string CodigoMec { get; set; }
+    public string Telefone { get; set; }
+    public string Email { get; set; }
+    public DateTime InicioAtividade { get; set; } = new();
+    public string CategoriaAdministrativa { get; set; }
+    public string OrganizacaoAcademica { get; set; }
+    public int IdEndereco { get; set; }
+    public string Estado { get; set; }
+}
