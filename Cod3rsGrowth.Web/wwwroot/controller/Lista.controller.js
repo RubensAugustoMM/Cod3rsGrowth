9158216a172d943737e9aa9c09560066d5159f7f
RubensAugustoMM <rubensammiranda@gmail.com>
2024-08-02 15:51:33 -0300

    task-implementar-pasta-para-recursos-e-tela-de-listagem-com-testes-OPA: Confiugra i18n e _onRouteMatched para exibir o nome do titulo da página correto /#time 1h 30m

--- Cod3rsGrowth.Web/wwwroot/controller/Lista.controller.js
+++ Cod3rsGrowth.Web/wwwroot/controller/Lista.controller.js
@@ -5,6 +5,67 @@ sap.ui.define([
 
     return Controller.extend("ui5.cod3rsgrowth.controller.Lista", {
         onInit() {
-		}
+			const oRouter = this.getOwnerComponent().getRouter();
+
+			oRouter.getRoute("Convenios").attachMatched(this._onRouteMatched, this);
+			oRouter.getRoute("Empresas").attachMatched(this._onRouteMatched, this);
+			oRouter.getRoute("Escolas").attachMatched(this._onRouteMatched, this);
+			oRouter.getRoute("Enderecos").attachMatched(this._onRouteMatched, this);
+		},
+
+		_onRouteMatched(oEvent) {
+			var nomeRota = oEvent.getParameter("name");
+			console.log("Rota ativada:", nomeRota);
+
+            var i18n = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+
+            var titulo;
+
+			switch (nomeRota) {
+			  case "Convenios":
+				titulo = i18n.getText("tituloConvenios");
+				this.byId("lista").setTitle(titulo)
+				this._handleConveniosRoute();
+				break;
+			  case "Empresas":
+				titulo = i18n.getText("tituloEmpresas");
+				this.byId("lista").setTitle(titulo)
+				this._handleEmpresasRoute();
+				break;
+			  case "Escolas":
+				titulo = i18n.getText("tituloEscolas");
+				this.byId("lista").setTitle(titulo)
+				this._handleEscolasRoute();
+				break;
+			  case "Enderecos":
+				titulo = i18n.getText("tituloEnderecos");
+				this.byId("lista").setTitle(titulo)
+				this._handleEnderecosRoute();
+				break;
+			  default:
+				console.log("Rota não reconhecida.");
+			}
+		  },
+	  
+		  _handleConveniosRoute() {
+			// Lógica específica para a rota 'convenios'
+			console.log("Tratando a rota de Convênios.");
+		  },
+	  
+		  _handleEmpresasRoute: function() {
+			// Lógica específica para a rota 'empresas'
+			console.log("Tratando a rota de Empresas.");
+		  },
+	  
+		  _handleEscolasRoute: function() {
+			// Lógica específica para a rota 'escolas'
+			console.log("Tratando a rota de Escolas.");
+		  },
+	  
+		  _handleEnderecosRoute: function() {
+			// Lógica específica para a rota 'enderecos'
+			console.log("Tratando a rota de Endereços.");
+		  }
+	  
 	});
 });
