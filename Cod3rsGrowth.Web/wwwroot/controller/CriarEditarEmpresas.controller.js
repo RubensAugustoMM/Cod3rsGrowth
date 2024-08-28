e85c45a03033b7aeda14835cf6c77c82f47ee638
RubensAugustoMM <rubensammiranda@gmail.com>
2024-08-28 18:03:11 -0300

    task-implementar-tela-de-criacao-para-registro-pai-com-testes-OPA: implementa botão de retorno /#time 0h 8m

--- Cod3rsGrowth.Web/wwwroot/controller/CriarEditarEmpresas.controller.js
+++ Cod3rsGrowth.Web/wwwroot/controller/CriarEditarEmpresas.controller.js
@@ -1,15 +1,30 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
 ], function(
-	Controller
+	Controller,
+	History
 ) {
 	"use strict";
 
 	return Controller.extend("ui5.cod3rsgrowth.controller.CriarEditarEmpresas", {
-
+		_nomeRotaAtual: "",
         onInit() {
             
         
-        }
+        },
+
+		aoPressionarBotaoDeNavegacao() {
+			const historico = History.getInstance();
+			const hashAnterior = historico.getPreviousHash();
+
+			if (hashAnterior != undefined) {
+				window.history.go(-1);
+			}
+			else {
+				const roteador = this.getOwnerComponent().getRouter();
+				roteador.navTo("Empresas", {}, {}, true);
+			}
+		}
 	}); 
 });
